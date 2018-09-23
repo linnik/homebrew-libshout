b9b4dc3482e7cb8ea3c784eae49e0b133e657838
Vyacheslav Linnik <vyacheslav.linnik@gmail.com>
2018-09-23 15:08:16 +0300

    add formula

---
+++ libshout.rb
@@ -0,0 +1,17 @@
+class Libshout < Formula
+  desc "Data and connectivity library for the icecast server"
+  homepage "https://icecast.org/"
+  url "https://downloads.xiph.org/releases/libshout/libshout-2.3.1.tar.gz"
+  sha256 "cf3c5f6b4a5e3fcfbe09fb7024aa88ad4099a9945f7cb037ec06bcee7a23926e"
+
+  depends_on "pkg-config" => :build
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "speex"
+  depends_on "theora"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
